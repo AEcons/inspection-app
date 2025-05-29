c9541aaac2ba0882d988cd58531d982194b1ef84
AEcons <aecons2024@gmail.com>
2025-05-29 11:10:20 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
-// PWA service worker
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('ae-inspector-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/record.html',
+        '/report.html',
+        '/detail.html',
+        '/summary-report.html',
+        '/style.css',
+        '/app.js',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
