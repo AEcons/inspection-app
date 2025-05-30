0dff2f962970c5d02cc62ee1390e80957491d235
AEcons <aecons2024@gmail.com>
2025-05-30 15:55:23 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,46 @@
-self.addEventListener('install', function(event) {});
+const CACHE_NAME = 'ae-inspector-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/record.html',
+  '/report.html',
+  '/detail.html',
+  '/summary-report.html',
+  '/style.css',
+  '/app.js',
+  '/print.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
